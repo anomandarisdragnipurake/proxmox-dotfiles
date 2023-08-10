296f613762d91d68ecc0b17092ea45ca64217b38
anomandarisdragnipurake <26286846+anomandarisdragnipurake@users.noreply.github.com>
2023-08-10 16:16:25 +0100

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,3 +10,18 @@ vim.api.nvim_set_keymap("n", "<Leader>t", ":UndotreeToggle<CR>", { noremap = tru
 
 -- Find hidden files
 vim.api.nvim_set_keymap("n", "<Leader>fh", ":Telescope find_files hidden=true<CR>", { noremap = true })
+
+-- Toggle floating terminal
+vim.api.nvim_set_keymap("n", "<Leader>m", ":ToggleTerm direction=float<CR>", { noremap = true })
+
+---- nvim-tmux-navigator
+--vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTmuxNavigateLeft<CR>", { noremap = true })
+--vim.api.nvim_set_keymap("n", "<C-j>", ":NvimTmuxNavigateDown<CR>", { noremap = true })
+--vim.api.nvim_set_keymap("n", "<C-k>", ":NvimTmuxNavigateUp<CR>", { noremap = true })
+--vim.api.nvim_set_keymap("n", "<C-l>", ":NvimTmuxNavigateRight<CR>", { noremap = true })
+
+-- Resize Window Panes
+vim.api.nvim_set_keymap("n", "<C-S-h>", ":vertical resize -2<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-S-j>", ":resize -2<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-S-k>", ":resize +2<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-S-l>", ":vertical resize +2<CR>", { noremap = true })
