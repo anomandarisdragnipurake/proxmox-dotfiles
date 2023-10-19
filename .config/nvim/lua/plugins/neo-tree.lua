7dcc2562ff4ee7a5078f974207f7af34d2bb30e0
anomandarisdragnipurake <26286846+anomandarisdragnipurake@users.noreply.github.com>
2023-10-19 11:54:46 +0100

    Neo-tree custom keybinds
    Neo-tree custom keybinds

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+        ["l"] = "open",
+        ["L"] = "focus_preview",
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+    },
+  },
+}
