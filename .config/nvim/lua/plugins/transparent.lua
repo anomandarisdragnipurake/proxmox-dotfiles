e4698c5c38e454152c420a8600f0a2a6df2f2c6c
anomandarisdragnipurake <26286846+anomandarisdragnipurake@users.noreply.github.com>
2023-11-29 11:03:16 +0000

    Transparency plugin

---
+++ .config/nvim/lua/plugins/transparent.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    config = {
+      extra_groups = {
+        "FloatBorder",
+        "LSPInfoBorder",
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+        "NormalFloat",
+        "NotifyDEBUGBody",
+        "NotifyDEBUGBorder",
+        "NotifyERRORBody",
+        "NotifyERRORBorder",
+        "NotifyINFOBody",
+        "NotifyINFOBorder",
+        "NotifyTRACEBody",
+        "NotifyTRACEBorder",
+        "NotifyWARNBody",
+        "NotifyWARNBorder",
+        "TelescopeBorder",
+        "TelescopePreviewNormal",
+        "TelescopePromptNormal",
+        "TelescopeResultsNormal",
+        "WhichKeyFloat",
+      },
+      exclude_groups = {},
+    },
+    keys = {
+      {
+        "<Leader>uo",
+        "<Cmd>TransparentToggle<Cr>",
+        desc = "Toggle Transparency",
+      },
+    },
+  },
+}
